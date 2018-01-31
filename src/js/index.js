3326f2d392940f85114a5cc9e4ad9fd756b8023a
Adri Skrtic <adri.curtis@gmail.com>
2018-01-31 15:30:35 -0600

    initial checkin for base framework

---
+++ src/js/index.js
@@ -0,0 +1,4 @@
+import numeral from 'numeral';
+/* eslint-disable no-console */
+const courseValue = numeral(1000).format('$0,0.00');
+console.log(`paid ${courseValue}`); // es-lint-disable-line no-console
